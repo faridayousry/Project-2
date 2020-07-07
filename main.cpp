bc6b1bf8207efe79d5b91f4831be204835073a08
faridayousry <59805894+faridayousry@users.noreply.github.com>
2020-07-07 13:55:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -64,6 +64,8 @@ int simulate(unsigned short instr)
 			    Regs[rd] = (Regs[rs] >> offset5 );			//NEEDS TO CHANGE TO CHECK SIGN BIT!!!!!!!!!!!!!!!!!!!!!1
 			    cout << "\n \t R"<< rd << " has been updated";
 				break;
+		default:
+            		printf("UNKNOWN INSTR\n");
                 
                 
             } else { /*add/sub*/      // format 2
@@ -111,8 +113,7 @@ int simulate(unsigned short instr)
 				break;
 				
 			case 1: printf("cmp\tr%d, #%d\n", rd, offset8);		//cmp imm instruction
-			    (Regs[rd] =  offset8)? 1;0		//USE CPSR?????????????????????????/
-			    cout << "\n \t R"<< rd << " has been updated";
+			    //(Regs[r] =  offset8)? 1;0		//USE CPSR ??
 				break;
 				
 			case 2: printf("add\tr%d, #%d\n", rd, offset8);		//add imm instruction
@@ -124,13 +125,204 @@ int simulate(unsigned short instr)
 			    Regs[rd] =  Regs[rd] - offset8 ;
 			    cout << "\n \t R"<< rd << " has been updated";
 				break;
+		default:
+            		printf("UNKNOWN INSTR\n");
 			
 		break;    
 		    
 		    
-	case 2:		//formats 
+	case 2:		//formats 4, 5, 6, 7, 8
+		op = (instr >> 11) & 3;         //mask to get bits 11 &12 (opcode)
+		rd = instr & 7;                 //mask to get 3 rightmost bits (rd for formats 4,5,7,8) 
+		rs = (instr >>  3) & 7;         //mask to get bits 3,4,5 (rs for formats 4,5  &  rb for 7,8)
+		offset5 = (instr >> 6) & 0x1F;  //mask to get bits 6-10 (offset5 for formats 1,9,10)
+				
+                switch(op){
+                    case 0: 		//formats 4 & 5     (ALU operations & Hi register ops/branch exchange)
+                        if ( (instr >> 10) & 1) == 0 {	   //format 4  
+                            op = (instr >> 6) & 15;	 //adjust op respective to be bits 6-9 for format 4 
+                            
+                            switch(op){
+                                case 0:		//and instruction (rd = rd & rs)
+                                        printf("and\tr%d, r%d\n", rd, rs);
+                                        Regs[rd] = Regs[rd] & Regs[rs];
+                                            cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                                case 1:     //eor instruction (rd = rd eor rs)
+                                        printf("eor\tr%d, r%d\n", rd, rs);
+                                        Regs[rd] = Regs[rd] eor Regs[rs];
+                                            cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                                case 2:		//lsl instruction (rd = rd << rs)
+                                        printf("lsl\tr%d, r%d\n", rd, rs);
+                                        Regs[rd] = Regs[rd] << Regs[rs];
+                                            cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                                case 3:     //lsr instruction (rd = rd >> rs)
+                                        printf("lsr\tr%d, r%d\n", rd, rs);
+                                        Regs[rd] = Regs[rd] >> Regs[rs];
+                                            cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                                 case 4:		//asr instruction (rd = rd asr rs)
+                                        printf("asr\tr%d, r%d\n", rd, rs);
+                                        Regs[rd] = Regs[rd] asr Regs[rs];
+                                            cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                                case 5:     //adc instruction (rd = rd + rs + c-bit)
+                                        printf("adc\tr%d, r%d\n", rd, rs);
+                                        Regs[rd] = Regs[rd] adc Regs[rs];
+                                            cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                                case 6:		//sbc instruction(rd = rd + rs + NOT c-bit)
+                                        printf("sbc\tr%d, r%d\n", rd, rs);
+                                        Regs[rd] = Regs[rd] sbc Regs[rs];
+                                            cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                                case 7:     //ror instruction (rd = rd ror rs)
+                                        printf("ror\tr%d, r%d\n", rd, rs);
+                                        Regs[rd] = Regs[rd] ror Regs[rs];
+                                            cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                                case 8:		//tst instruction (rd = rd << rs)
+                                        printf("tst\tr%d, r%d\n", rd, rs);
+                                        //Regs[rd] = Regs[rd] tst Regs[rs];  - set condition codes
+                                            //cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                                case 9:     //neg instruction (rd = rd >> rs)
+                                        printf("neg\tr%d, r%d\n", rd, rs);
+                                        Regs[rd] = -Regs[rs];
+                                            cout << "\n \t R"<< rd << " has been updated";
+                                            break;
+
+                            }	
+                                break;
+                            default:
+                                        printf("UNKNOWN INSTR\n");
+                            }
+                        
+                        else{				//format 5      //NOT FULLY SURE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                            op = (instr >> 8) & 3;	 //adjust op respective to be bits 8-9 for format 5
+                            int hs = rs;
+                            int hd = rd;
+                            int H = (instr >> 6) & 3;   //mask to get H1 and H2 combined in 1 variable
+
+                            switch(op){
+                                case 0:		//add instructions hi & lo 
+
+                                        if( H == 1)     //ADD Rd, Hs
+                                        {
+                                            printf("add\tr%d, r%d\n", rd, hs);
+                                            Regs[rd] = Regs[rd] + Regs[hs];
+                                                cout << "\n \t R"<< rd << " has been updated";
+                                        }
+
+                                        else if( H == 2)     //ADD Hd, Rs
+                                        {
+                                            printf("add\tr%d, r%d\n", hd, rs);
+                                            Regs[hd] = Regs[hd] + Regs[rs];
+                                                cout << "\n \t R"<< hd << " has been updated";
+                                        }
+
+                                         else    //H = 3 ; ADD Hd, Hs
+                                        {
+                                            printf("add\tr%d, r%d\n", hd, hs);
+                                            Regs[hd] = Regs[hd] + Regs[hs];
+                                                cout << "\n \t R"<< hd << " has been updated";
+                                        }
+
+
+                                            break;
+
+                                case 1:    //cmp instructions hi & lo 
+
+                                        if( H == 1)     //cmp Rd, Hs
+                                        {
+                                            printf("cmp\tr%d, r%d\n", rd, hs);
+                                            //Regs[rd] == Regs[hs]? 0:1
+                                                //cout << "\n \t R"<< rd << " has been updated";
+                                        }
+
+                                        else  if( H == 2)     //cmp hd, rs
+                                        {
+                                            printf("cmp\tr%d, r%d\n", hd, rs);
+                                            //Regs[hd] == Regs[rs]? 0:1
+                                                //cout << "\n \t R"<< hd << " has been updated";
+                                        }
+
+                                         else     //H == 3 ; cmp hd, hs
+                                        {
+                                            printf("cmp\tr%d, r%d\n", hd, hs);
+                                            //Regs[hd] == Regs[hs]? 0:1
+                                                //cout << "\n \t R"<< hd << " has been updated";
+                                        }
+
+
+                                            break;
+
+                                case 2:		//mov instructions hi & lo 
+                                        if (H == 1){        //mov rd, hs
+                                            printf("mov\tr%d, r%d\n", rd, hs);
+                                            Regs[rd] = Regs[hs] ;
+                                                cout << "\n \t R"<< rd << " has been updated";
+                                        }
+                                        else if (H == 2){        //mov hd, rs
+                                            printf("mov\tr%d, r%d\n", hd, rs);
+                                            Regs[hd] = Regs[rs] ;
+                                                cout << "\n \t R"<< hd << " has been updated";
+                                        }
+                                        else{        // H == 2 ; mov hd, hs
+                                            printf("mov\tr%d, r%d\n", hd, hs);
+                                            Regs[hd] = Regs[hs] ;
+                                                cout << "\n \t R"<< hd << " has been updated";
+                                        }
+
+                                                break;
+
+                                case 3:    //bx instructions hi & lo 
+                                        if(H == 0){     //BX rs
+                                            printf("bx\tr%d\n", rs);
+                                            // UPDATE PC   ??????????????????????????????????????????????
+                                            //PC = PC + rs ?????
+                                                //cout << "\n \t PC has been updated";
+                                        }
+                                        else{   //H == 11; BX hs
+                                            printf("bx\tr%d\n", hs);
+                                            // UPDATE PC   ??????????????????????????????????????????????
+                                            //PC = PC + hs ?????
+                                                //cout << "\n \t PC has been updated";
+
+                                        }
+
+                                                break;
+
+                            }	
+                                break;
+                            default:
+                                        printf("UNKNOWN INSTR\n");
+                            }
+
+
+
+                    case 1:     //format 6
+
+
+
+            }
+		        default:
+            		printf("UNKNOWN INSTR\n");
+				
 		break;   
 		    
+            
 		    
 	case 3:		//formats 
 		break;  
